e26cc82d1fda2cf1b8f9374a5a025b67b36506d8
jeremiasianigro <ianigroj@gmail.com>
2024-05-09 12:14:35 -0300

    .

--- clases.php
+++ clases.php
@@ -1,4 +1,14 @@
 <?php
+class presupuestos
+{
+    //clase q instancia los presupuestos
+    // funcion buscar clientes en tabla
+
+    // funcion q crea presupuestos en borrador
+    // funcion q busca un presupuesto
+    // funcion q da de baja presupuesto
+    // funcion q cambia de estado y lo modifica
+}
 
 class cliente
 {
@@ -50,6 +60,7 @@ class insumo
     protected $importe;
     protected $comprobante;
 
+
     function __construct ($id_presup,$insumo)
     {
         $this->id_presup = $id_presup;
@@ -82,21 +93,87 @@ class presupuesto
 
     private $datosDB;
     protected $id_presup;
-    protected $cliente;
+    protected $estado;
+    protected $cuil;
+    protected $nombre;
     protected $insumos=[];
+    protected $insumos_instanciados=[];
     protected $total;
 
-    function __construct($cuil,$insumos)
+    function __construct($cliente,$insumos,$estado)
     {
-        
+       $datosDB=new datosDB;
+       $this->datosDB = $datosDB;
+       $presupuestos= $datosDB->leer_tabla_presupuestos_borrador();
+       $id=0;
+       foreach ($presupuestos as $un_presupuesto)
+        {
+            $id=$un_presupuesto["id_presp"];
+        }
+        $id_presup=$id+1;
+        $this->id_presup=$id_presup;
+        $this->estado=$estado;
+        $this->cuil=$cliente["cuil"];
+        $this->nombre=$cliente["nombre"];
+        $this->insumos=$insumos;
+
+        if (is_array($this->insumos())) 
+        {
+            
+        }
+
+
+
+
+
     }
+// GETERS
+function datosDB()
+{
+    return $this->datosDB;
 }
+function id_presup()
+{
+    return $this->id_presup;
+}
+function estado()
+{
+    return $this->estado;
+}
+function cuil()
+{
+    return $this->cuil;
+}
+function nombre()
+{
+    return $this->nombre;
+}
+function insumos()
+{
+    return $this->insumos;
+}
+function total()
+{
+    return $this->total;
+}
+
+}// fin de la clase
 
 class datosDB
 {
     function leer_tabla_clientes()
     {
         $textoDelArchivo = file_get_contents("tabla_clientes.json");
+        return json_decode($textoDelArchivo,true);
+    }
+    function leer_tabla_presupuestos_borrador()
+    {
+        $textoDelArchivo = file_get_contents("tabla_presupuestos_borrador.json");
+        return json_decode($textoDelArchivo,true);
+    }
+    function leer_tabla_insumos_variables()
+    {
+        $textoDelArchivo = file_get_contents("tabla_insumos_variables.json");
         return json_decode($textoDelArchivo,true);
     }
 }
--- de_array_a_json.php
+++ de_array_a_json.php
@@ -20,9 +20,63 @@ $clientes = [
         "cuil"=>"20-30899144-8",
         "cbu"=>12345656985555,]
 ];
+*/
+
+/*
+$presupuestos= [
+    [
+        "id_presup" =>1,
+        "estado"=>"borrador",
+        "cuil"=>"20-30899144-8",
+        "nombre"=>"Sebastian Toyos",
+        "total"=>10000
+    ],
+    [
+        "id_presup" =>2,
+        "estado"=>"borrador",
+        "cuil"=>"20-32171890-7",
+        "nombre"=>"Jeremias Ianigro",
+        "total"=>20000
+    ],
+    [
+        "id_presup" =>1,
+        "estado"=>"borrador",
+        "cuil"=>"20-31163877-8",
+        "nombre"=>"Pablo Pugliese",
+        "total"=>30000
+    ]
+];
+
+*/
+
+/*
+$insumos=[
+    [
+        "id_presup"=>1,
+        "concepto"=>"manguera",
+        "importe"=>10000,
+        "comprobante"=>true
+    ],
+    [
+        "id_presup"=>1,
+        "concepto"=>"clavos",
+        "importe"=>500,
+        "comprobante"=>false
+    ],
+    [
+        "id_presup"=>1,
+        "concepto"=>"chapa",
+        "importe"=>5000,
+        "comprobante"=>false
+    ],
+    [
+        "id_presup"=>1,
+        "concepto"=>"garrafa",
+        "importe"=>70000,
+        "comprobante"=>true
+    ]
+];
 
 
-
-
-file_put_contents("tabla_clientes.json",json_encode($clientes,JSON_PRETTY_PRINT));
+file_put_contents("tabla_insumos_variables.json",json_encode($insumos,JSON_PRETTY_PRINT));
 */
